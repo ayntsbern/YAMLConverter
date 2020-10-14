397341a303c446704c0232bfa184338492a2f2a9
Yulia <>
2020-10-14 10:34:30 +0300

    Initial commit

---
+++ src/main/java/com/ayntsbern/YAMLConverter/YamlConverterApplication.java
@@ -0,0 +1,13 @@
+package com.ayntsbern.YAMLConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayntsbern/YAMLConverter/YamlConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayntsbern.YAMLConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
